ba65c1f25cbd26af9aecbc80c71538aad95a085a
AlejandroPinto99 <a.pinto8899@gmail.com>
2025-04-19 18:14:18 -0600

    Adding navbar for mobile

--- src/components/Navbar/navBarMobile.js
+++ src/components/Navbar/navBarMobile.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './navbar.css'
-import { Bars3Icon } from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function NavbarMobile() {
     const [isOpen, setIsOpen] = useState(false);
@@ -10,12 +10,17 @@ export default function NavbarMobile() {
                 !isOpen ? (
                     <Bars3Icon onClick={() => setIsOpen(true)} className="navbar-mobile" />
                 ) : (
+                    <div>
+                        <button onClick={() => setIsOpen(false)}>
+                            <XMarkIcon style={{ color: '#fff', height: '25px', width: '25px', position: 'absolute', top: 10, right: 10, zIndex: 2 }} />
+                        </button>
                         <ul className="open-mobile-menu">
-                            <li><a href='#home'>Inicio</a></li>
-                            <li><a href='#destinos'>Destinos</a></li>
-                            <li><a href='#buses'>Autobuses</a></li>
-                            <li><a href='#servicios'>Servicios</a></li>
+                            <li onClick={() => setIsOpen(false)}><a href='#home' >Inicio</a></li>
+                            <li onClick={() => setIsOpen(false)}><a href='#destinos' >Destinos</a></li>
+                            <li onClick={() => setIsOpen(false)}><a href='#buses' >Autobuses</a></li>
+                            <li onClick={() => setIsOpen(false)}><a href='#servicios' >Servicios</a></li>
                         </ul>
+                    </div>
                 )
             }
         </div>
